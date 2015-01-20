076c0f1fbeb5d7f7222892091762078d8c29123e
Diana Sirenko <dianasirenko@gmail.com>
2015-01-20 12:36:51 +0200

    add main menu animation

--- Classes/Scenes/MainMenu.cpp
+++ Classes/Scenes/MainMenu.cpp
@@ -67,48 +67,108 @@ bool MainMenu::init()
     showBackground(BackgroundType::Light);
     std::string image_name;
 
-    //logo
+    ///////////////////////////////////
+    //english word
     image_name = "main-menu/name_en.png";
     CCSprite* logo = CCSprite::create(image_name.c_str());
 
-    logo->setPositionX(ORIGIN.x + VISIBLE_SIZE.width*0.7f);
-    logo->setPositionY(ORIGIN.y + VISIBLE_SIZE.height*0.6f);
+    logo->setPosition(ccp(ORIGIN.x - 200,
+                          ORIGIN.y + VISIBLE_SIZE.height*0.6f));
     this->addChild(logo);
+    logo->runAction(CCSequence::create(
+                     CCDelayTime::create(2.9f),
+                     CCEaseElasticOut::create(
+                        CCMoveTo::create(1.4f,
+                                         ccp(ORIGIN.x + VISIBLE_SIZE.width*0.7f,
+                                             ORIGIN.y + VISIBLE_SIZE.height*0.6f))
+                         ),
+                     NULL
+                 ));
 
     //////////////////////////////////////////////////////////
-    //main logo
+    //LEARN word
+
+    float letter_y = ORIGIN.y+VISIBLE_SIZE.height*0.9;
+    float letter_scale = (VISIBLE_SIZE.height/6);
+
     CCSprite* l = CCSprite::create("main-menu/l.png");
     l->setPosition(ccp(ORIGIN.x+VISIBLE_SIZE.width*0.2,
-                             ORIGIN.y+VISIBLE_SIZE.height*0.85));
+                             ORIGIN.y+VISIBLE_SIZE.height+100));
     this->addChild(l);
-//    l->runAction(CCRepeatForever::create(
-//                               CCSequence::create(
-//                                    (CCRotateTo::create(0.3,-10.0f)),
-//                                    (CCRotateTo::create(0.6,+20.0f)),
-//                                    (CCRotateTo::create(0.3,-10.0f)),
-//                                    NULL)
-//                               )
-//                            );
+    l->setScale(letter_scale/l->getContentSize().height);
+    l->runAction(CCSequence::create(
+                     CCDelayTime::create(1.0f),
+                     CCEaseElasticOut::create(
+                        CCMoveTo::create(1.0f,
+                                         ccp(ORIGIN.x+VISIBLE_SIZE.width*0.2,
+                                             letter_y))
+                         ),
+                     NULL
+                 ));
+    ///////////////////////////////////
 
     CCSprite* e = CCSprite::create("main-menu/e.png");
     e->setPosition(ccp(ORIGIN.x+VISIBLE_SIZE.width*0.35,
-                             ORIGIN.y+VISIBLE_SIZE.height*0.85));
+                             ORIGIN.y+VISIBLE_SIZE.height+100));
     this->addChild(e);
+    e->setScale(letter_scale/e->getContentSize().height);
+    e->runAction(CCSequence::create(
+                     CCDelayTime::create(1.4f),
+                     CCEaseElasticOut::create(
+                        CCMoveTo::create(1.0f,
+                                         ccp(ORIGIN.x+VISIBLE_SIZE.width*0.35,
+                                             letter_y))
+                         ),
+                     NULL
+                 ));
 
+    ///////////////////////////////
     CCSprite* a = CCSprite::create("main-menu/a.png");
     a->setPosition(ccp(ORIGIN.x+VISIBLE_SIZE.width*0.5,
-                             ORIGIN.y+VISIBLE_SIZE.height*0.85));
+                             ORIGIN.y+VISIBLE_SIZE.height+100));
     this->addChild(a);
+    a->setScale(letter_scale/a->getContentSize().height);
+    a->runAction(CCSequence::create(
+                     CCDelayTime::create(1.8f),
+                     CCEaseElasticOut::create(
+                        CCMoveTo::create(1.0f,
+                                         ccp(ORIGIN.x+VISIBLE_SIZE.width*0.5,
+                                             letter_y))
+                         ),
+                     NULL
+                 ));
 
+    ////////////////////////////////
     CCSprite* r = CCSprite::create("main-menu/r.png");
     r->setPosition(ccp(ORIGIN.x+VISIBLE_SIZE.width*0.65,
-                             ORIGIN.y+VISIBLE_SIZE.height*0.85));
+                             ORIGIN.y+VISIBLE_SIZE.height+100));
     this->addChild(r);
+    r->setScale(letter_scale/r->getContentSize().height);
+    r->runAction(CCSequence::create(
+                     CCDelayTime::create(2.2f),
+                     CCEaseElasticOut::create(
+                        CCMoveTo::create(1.0f,
+                                         ccp(ORIGIN.x+VISIBLE_SIZE.width*0.65,
+                                             letter_y))
+                         ),
+                     NULL
+                 ));
 
+    ////////////////////////////////
     CCSprite* n = CCSprite::create("main-menu/n.png");
     n->setPosition(ccp(ORIGIN.x+VISIBLE_SIZE.width*0.8,
-                             ORIGIN.y+VISIBLE_SIZE.height*0.85));
+                             ORIGIN.y+VISIBLE_SIZE.height+100));
     this->addChild(n);
+    n->setScale(letter_scale/n->getContentSize().height);
+    n->runAction(CCSequence::create(
+                     CCDelayTime::create(2.6f),
+                     CCEaseElasticOut::create(
+                        CCMoveTo::create(1.0f,
+                                         ccp(ORIGIN.x+VISIBLE_SIZE.width*0.8,
+                                             letter_y))
+                         ),
+                     NULL
+                 ));
 
     ///////////////////////////////////////////////////////////
     //menu
@@ -129,7 +189,6 @@ bool MainMenu::init()
 
     //animation
     button_play->runAction(CCSequence::create(
-                               CCDelayTime::create(0.2f),
                                CCEaseElasticOut::create(
                                  CCMoveTo::create(1.0f,button_play_position)),
                                NULL
@@ -139,14 +198,22 @@ bool MainMenu::init()
     CCSprite* button_settings_image = CCSprite::create("main-menu/settings-button.png");
     ADMenuItem* button_settings = ADMenuItem::create(button_settings_image);
     float padding = 35/SCALE;
-    button_settings->setPositionX(
-                ORIGIN.x + VISIBLE_SIZE.width
-                - padding
-                - button_settings->getContentSize().width*0.5f);
-    button_settings->setPositionY(ORIGIN.y + padding +
-                                  button_settings->getContentSize().height*0.5f);
+
+    CCPoint button_settings_position = (ccp(ORIGIN.x + VISIBLE_SIZE.width - padding - button_settings->getContentSize().width*0.5f,
+                                            ORIGIN.y + padding + button_settings->getContentSize().height*0.5f));
+
+    button_settings->setPosition(ccp(button_settings_position.x,
+                                     ORIGIN.y + VISIBLE_SIZE.height + button_settings->getContentSize().height/2));
     CONNECT(button_settings->signalOnClick, this, &MainMenu::onSettingsClick);
     menu->addChild(button_settings);
+
+    //animation
+    button_settings->runAction(CCSequence::create(
+                               CCDelayTime::create(0.6f),
+                               CCEaseElasticOut::create(
+                                 CCMoveTo::create(1.0f,button_settings_position)),
+                               NULL
+                           ));
 
     return true;
 }
