044e24f164e7397528de892def5a67ec281b960e
Andrii Chaika <andriy@chaika.in.ua>
2015-02-02 18:00:29 +0200

    ios project configured

--- build.win32.py
+++ build.win32.py
@@ -6,7 +6,7 @@ project = ResourceOnlyProject(env)
 project.resource_output = os.path.join(working_directory, 'Resources')
 project.resource_task = createResourceCompilerTask(
     os.path.join(working_directory, 'Resources_storage'),
-    'kids-medium',
+    'kids-basic1',
     'android_win',
     ['en']
 )
