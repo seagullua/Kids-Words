ffa2a0f73994dbbeda0c098559559060df92c305
Andrii Chaika <andriy@chaika.in.ua>
2015-02-03 20:00:08 +0200

    Windows Project

---
+++ proj.wp8/wp8/HelloCpp/HelloCppComponent/PlatformImpl.cpp
@@ -0,0 +1,9 @@
+//#include <ADLib\PlatformImpl\ADAds_NoAds.hpp>
+//#include <ADLib\PlatformImpl\ADBrowser_None.hpp>
+//#include <ADLib\PlatformImpl\ADInApp_None.hpp>
+//#include <ADLib\PlatformImpl\ADInfo_Default.hpp>
+//#include <ADLib\PlatformImpl\ADLanguage_Default.hpp>
+//#include <ADLib\PlatformImpl\ADNotification_None.hpp>
+//#include <ADLib\PlatformImpl\ADStatistics_None.hpp>
+#include <ADLib\PlatformImpl\ADVirtualCurrency_None.hpp>
+#include <ADLib\PlatformImpl\ADPushWoosh_None.hpp>
